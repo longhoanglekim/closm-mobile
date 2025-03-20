5439be9d9bba0099bb60adec412f39d1442ff694
ntdungg15 <ngtiendung0105204@gmail.com>
2025-03-20 17:20:27 +0700

    update category

--- components/homeComponent/category/Category.tsx
+++ components/homeComponent/category/Category.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ListImage from "../../ListImage/ListImage";
 import { Text, View } from "react-native";
+import home from "@/constants/home"
 
 const Category = () => {
   return (
     <View>
       <Text>Category</Text>
+      <View style={home.category}>
+<View style={home.categoryItem}></View>
+
+      </View>      
+      <View>
+
+
+      </View>
       <ListImage />
     </View>
   );
---
+++ constants/home.ts
@@ -0,0 +1,19 @@
+import Category from "@/components/homeComponent/category/Category";
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    category: {
+        width: 165,
+        height: 192,
+        flexShrink: 0,
+        backgroundColor: "#F5F5F5",
+        borderRadius: 9,
+    },
+    categoryItem: {
+        width:75,
+        height:75,
+        backgroundColor:"blue"
+
+    }
+
+});
