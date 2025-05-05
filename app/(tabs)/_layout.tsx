82e42d35850778410b9736a7ff7711cf8d74a16c
ntdungg15 <ngtiendung0105204@gmail.com>
2025-05-05 12:03:51 +0700

    add icon app tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,7 +5,7 @@ import { Platform, ViewStyle } from "react-native";
 import { HapticTab } from "@/components/HapticTab";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
-
+import { Image } from "react-native";
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
@@ -20,24 +20,68 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={{ uri: "https://res.cloudinary.com/dwddrjz3b/image/upload/v1746421333/home-button_sbyo9y.png" }}
+              style={{
+                width: size,
+                height: size,
+                 // Áp dụng màu sắc nếu cần
+              }}
+              resizeMode="contain"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="cart"
         options={{
           title: "Cart",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={{ uri: "https://res.cloudinary.com/dwddrjz3b/image/upload/v1746421333/shopping-cart_kes3ai.png" }}
+              style={{
+                width: size,
+                height: size,
+                
+              }}
+              resizeMode="contain"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="sales"
         options={{
           title: "Sale",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={{ uri: "https://res.cloudinary.com/dwddrjz3b/image/upload/v1746421333/sales_g4q09f.png" }}
+              style={{
+                width: size,
+                height: size,
+                
+              }}
+              resizeMode="contain"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={{ uri: "https://res.cloudinary.com/dwddrjz3b/image/upload/v1746421333/verified_oia5fq.png" }}
+              style={{
+                width: size,
+                height: size,
+                
+              }}
+              resizeMode="contain"
+            />
+          ),
         }}
       />
       <Tabs.Screen
